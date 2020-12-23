7f7b0512c7db16502d27cf68011291f4ba1dad25
rob tillaart <rob.tillaart@gmail.com>
2020-12-23 17:30:09 +0100

    initial arduino-ci

---
+++ test/unit_test_001.cpp
@@ -0,0 +1,105 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2020-12-03
+// PURPOSE: unit tests for the Fraction
+//          https://github.com/RobTillaart/Fraction
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// https://github.com/Arduino-CI/arduino_ci/blob/master/cpp/unittest/Assertion.h#L33-L42
+// ----------------------------
+// assertEqual(expected, actual)
+// assertNotEqual(expected, actual)
+// assertLess(expected, actual)
+// assertMore(expected, actual)
+// assertLessOrEqual(expected, actual)
+// assertMoreOrEqual(expected, actual)
+// assertTrue(actual)
+// assertFalse(actual)
+// assertNull(actual)
+// assertNotNull(actual)
+
+#include <ArduinoUnitTests.h>
+
+#define assertEqualFloat(arg1, arg2, arg3)  assertOp("assertEqualFloat", "expected", fabs(arg1 - arg2), compareLessOrEqual, "<=", "actual", arg3)
+#define assertEqualINF(arg)  assertOp("assertEqualINF", "expected", INFINITY, compareEqual, "==", "actual", arg)
+#define assertEqualNAN(arg)  assertOp("assertEqualNAN", "expected", true, compareEqual, "==", "actual", isnan(arg))
+
+
+#include "Arduino.h"
+#include "fraction.h"
+
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+/*
+unittest(test_new_operator)
+{
+  assertEqualINF(exp(800));
+  assertEqualINF(0.0/0.0);
+  assertEqualINF(42);
+  
+  assertEqualNAN(INFINITY - INFINITY);
+  assertEqualNAN(0.0/0.0);
+  assertEqualNAN(42);
+}
+*/
+
+unittest(test_constructor)
+{
+  Fraction pi(PI);
+  assertEqual(1, pi.nominator());
+  assertEqual(1, pi.denominator());
+  assertFalse(pi.isProper());
+  fprintf(stderr, "PI -> %1.6f\n", pi.toFloat());
+  
+  Fraction ee(EULER);
+  assertEqual(1, ee.nominator());
+  assertEqual(1, ee.denominator());
+  assertFalse(ee.isProper());
+  fprintf(stderr, "EULER -> %1.6f\n", ee.toFloat());
+
+  Fraction fr(49, 14);
+  assertEqual(7, fr.nominator());
+  assertEqual(2, fr.denominator());
+  assertFalse(fr.isProper());
+}
+
+
+unittest(test_comparisons)
+{
+  Fraction pi(PI);
+  Fraction ee(EULER);
+  assertTrue(pi >  ee);
+  assertTrue(pi >= ee);
+  assertTrue(ee <  pi);
+  assertTrue(ee <= pi);
+  assertTrue(ee != pi);
+  assertFalse(ee == pi);
+
+  Fraction mpi = -pi;
+  assertEqual(mpi.nominator(), -pi.nominator());
+  assertEqual(mpi.denominator(), pi.denominator());
+}
+
+
+unittest(test_math)
+{
+  Fraction pi(PI);
+  Fraction ee(EULER);
+  // TODO
+}
+
+
+unittest_main()
+
+// --------
