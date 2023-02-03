7c73d71724bdd161bda9ca16f5bc0ad786429cee
rob tillaart <rob.tillaart@gmail.com>
2023-02-03 13:02:16 +0100

    fix build

--- examples/Fraction_performance/Fraction_performance.ino
+++ examples/Fraction_performance/Fraction_performance.ino
@@ -68,7 +68,7 @@ void test_math()
   Serial.println(__FUNCTION__);
 
   delay(10);
-  Fraction a((int)0);
+  Fraction a(0, 1);
   Fraction pi(PI);
   Fraction e(EULER);
   start = micros();
@@ -143,7 +143,7 @@ void  test_misc()
   Serial.println(__FUNCTION__);
 
   delay(10);
-  Fraction a((int)0);
+  Fraction a(0, 1);
   Fraction pi(PI);
   Fraction e(EULER);
   start = micros();
